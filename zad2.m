98a173b6cc9df71d19b6958872cd052b3132a067
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-03-13 19:22:19 +0000

    Update zad2.m

--- zad2.m
+++ zad2.m
@@ -1,22 +1,20 @@
 clear
 
-function fun = fun1(x)
-  fun=3*x.^3-4*x.^2-1;
-end
+f=@(x) 3*x.^3-4*x.^2-1;
 
 a = 0;
 b = 2;
 y = a:0.1:b;
 w = [];
 
-if fun1(a)*fun1(b) >= 0
+if f(a)*f(b) >= 0
   printf("Brak rozwiazania na przedziale\n");
   
 else
    while b-a>0.001
       wartosc = (a+b)/2 
       w(end+1) = wartosc;  
-     if fun1(wartosc)*fun1(a) > 0
+     if f(a)*f(a) > 0
        a=wartosc;
      else
        b=wartosc;
