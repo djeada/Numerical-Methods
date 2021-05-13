4924ef0ddf55fb9187245ca3a53b696b69c33515
djeada <addjellouli1@gmail.com>
2021-05-14 01:35:22 +0200

    added ode methods

---
+++ src/euler.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+#x(t + h) = x(t) + hf(x,t) 
+
+def f(x,t): return x*np.cos(t)
+
+h = 0.01
+t_values = np.arange(0, 2*np.pi, h)
+x_values = []
+
+x = 0
+
+for t in t_values:
+    x_values.append(x)
+    x = x + h*f(x,t)
+
+plt.plot(t_values, np.sin(t_values))
+plt.plot(t_values, x_values, ".")
+plt.axhline(c="k")
+plt.show()
---
+++ src/runge_kutta.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# y' = u
+# u' = -y
+
+'''
+y'' + y = 0
+
+y(0) = 0 and y'(0) = 1/pi
+'''
+
+def F(y, u, x):
+    return -y
+
+a = 0
+b = 2*np.pi
+N =100
+h = (b-a)/N
+
+xpoints = np.arange(a,b,h)
+ypoints = []
+upoints = []
+
+y = 0.0
+u = 1./np.pi 
+
+for x in xpoints:
+    ypoints.append(y)
+    upoints.append(u)
+
+    m1 = h*u
+    k1 = h*F(y, u, x)  #(x, v, t)
+
+    m2 = h*(u + 0.5*k1)
+    k2 = h*F(y+0.5*m1, u+0.5*k1, x+0.5*h)
+
+    m3 = h*(u + 0.5*k2)
+    k3 = h*F(y+0.5*m2, u+0.5*k2, x+0.5*h)
+
+    m4 = h*(u + k3)
+    k4 = h*F(y+m3, u+k3, x+h)
+
+    y += (m1 + 2*m2 + 2*m3 + m4)/6
+    u += (k1 + 2*k2 + 2*k3 + k4)/6
+
+plt.plot(xpoints, ypoints)
+#plt.xlim([0,4])
+#plt.xlim([0,4])
+plt.show()
+
