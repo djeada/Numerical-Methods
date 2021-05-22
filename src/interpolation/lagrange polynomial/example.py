efb0677fd6f1b0f7a3ea692f97c4b09ed74ecdd6
djeada <addjellouli1@gmail.com>
2021-05-22 15:50:01 +0200

    modified interpolation examples

--- src/interpolation/lagrange polynomial/example.py
+++ src/interpolation/lagrange polynomial/example.py
@@ -1,20 +1,20 @@
-import numpy as np
 import matplotlib.pyplot as plt
-from cubic_spline import cubic_spline
+import numpy as np
+from lagrange_polynomial import lagrange_polynomial
 
 
 def main():
-    x = np.array([0, 1, 2, 3])
-    y = np.array([0, -1, 4, 3])
+    x = np.array([-9, -5, -2.5, 4, 7])
+    y = np.array([-2, 3, 0, 5, 11])
 
-    result_function = cubic_spline(x, y)
+    result_function = lagrange_polynomial(x, y)
 
-    x_new = np.arange(0, 5, 0.1)
+    x_new = np.arange(-10, 10, 0.1)
     y_new = np.array([result_function(i) for i in x_new])
 
     fig = plt.figure(figsize=(10, 8))
     plt.plot(x_new, y_new, "b", x, y, "ro")
-    plt.title("Cubic Spline")
+    plt.title("Lagrange Polynomial")
     plt.grid()
     plt.xlabel("x")
     plt.ylabel("y")
