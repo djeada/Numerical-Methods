e190ac58521fb107437ff9c6ce40db12d4fdae43
djeada <addjellouli1@gmail.com>
2021-05-24 14:05:26 +0200

    updated slides

--- src/regression/multiple_linear_regression/least_squares.py
+++ src/regression/multiple_linear_regression/least_squares.py
@@ -2,11 +2,12 @@ import numpy as np
 
 
 def least_squares(x, y):
-    a = np.array([np.array((elem, 1.)) for elem in x])
+    a = np.append(x, np.ones((len(x), 1)), axis=-1)
     y = y[:, np.newaxis]
 
     result = np.linalg.inv(np.dot(a.T, a))
     result = np.dot(result, a.T)
     result = np.dot(result, y)
 
-    return result[0][0], result[1][0]
+    return result[-1][0], result[:-1]
+
