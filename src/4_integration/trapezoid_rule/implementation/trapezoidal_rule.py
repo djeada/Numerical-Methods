ed18d15c3a410a9e5bff295ad2d812c6d3e3a58c
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-06-03 22:05:04 +0200

    Update and rename trapezoid_rule.py to trapezoidal_rule.py

---
+++ src/4_integration/trapezoid_rule/implementation/trapezoidal_rule.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def trapezoidal_rule(func, a, b, n):
+    """
+    Compute the approximate definite integral of a function using the trapezoidal rule.
+
+    Args:
+        func (callable): The function to integrate.
+        a (float): The lower limit of integration.
+        b (float): The upper limit of integration.
+        n (int): The number of subintervals.
+
+    Returns:
+        float: The approximate definite integral.
+    """
+    h = (b - a) / n
+    x = np.linspace(a, b, n+1)
+    y = func(x)
+
+    integral = (h / 2) * (np.sum(y[1:-1]) + y[0] + y[-1])
+
+    return integral
