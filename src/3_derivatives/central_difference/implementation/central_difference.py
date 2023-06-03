2894159e0df2606c3865286c942b58c4c9abf4b4
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-06-03 18:27:19 +0200

    Update central_difference.py

--- src/3_derivatives/central_difference/implementation/central_difference.py
+++ src/3_derivatives/central_difference/implementation/central_difference.py
@@ -1,6 +1,14 @@
-def central_difference(f, x, h):
-    fxph = f(x + h)
-    fxnh = f(x - h)
-    result = (fxph - fxnh) / (2 * h)
-    return result
 
+def central_difference(func, x, h):
+    """
+    Compute the approximate derivative of a function using the central difference method.
+
+    Args:
+        func (callable): The function for which to compute the derivative.
+        x (float): The point at which to approximate the derivative.
+        h (float): The step size.
+
+    Returns:
+        float: The approximate derivative of the function at the given point.
+    """
+    return (func(x + h) - func(x - h)) / (2 * h)
