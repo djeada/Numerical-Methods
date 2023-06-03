655c4a7bdd9888b373bb53a422471a017ac4ad08
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-06-03 18:24:41 +0200

    Update and rename temp.md to forward_difference.py

---
+++ src/3_derivatives/forward_difference/implementation/forward_difference.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def forward_difference(func, x, h):
+    """
+    Compute the approximate derivative of a function using the forward difference method.
+
+    Args:
+        func (callable): The function for which to compute the derivative.
+        x (float): The point at which to approximate the derivative.
+        h (float): The step size.
+
+    Returns:
+        float: The approximate derivative of the function at the given point.
+    """
+    return (func(x + h) - func(x)) / h
+
