2ae6b150141180aac969fa99b1638c49fdd35b2e
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-06-03 17:50:45 +0200

    Update relaxation_method.py

--- src/1_root_and_extrema_finding/relaxation_method/implementation/relaxation_method.py
+++ src/1_root_and_extrema_finding/relaxation_method/implementation/relaxation_method.py
@@ -1,23 +1,31 @@
 import numpy as np
 
 
-def f(x):
-    return x + np.exp(-2 * x) - 1
+def relaxation_method(func, x0, epsilon=1e-8, max_iter=100):
+    """
+    Relaxation Method for finding the fixed point of a function.
 
+    Args:
+        func (callable): The function for which to find the fixed point.
+        x0 (float): The initial guess for the fixed point.
+        epsilon (float): The desired accuracy of the solution.
+        max_iter (int): The maximum number of iterations.
 
-def dfdx(x):
-    return 1 - 2 * np.exp(-2 * x)
+    Returns:
+        float: The estimated fixed point of the function.
 
+    Raises:
+        ValueError: If the maximum number of iterations is reached without convergence.
+    """
+    x = x0
 
-def relaxation_method(f, x=1, num_iter=100, espilon=1e-8):
+    for _ in range(max_iter):
+        x_new = func(x)
 
-    values = []
-    for i in range(num_iter):
-        values = f(x)
-        x = f(x)
+        if abs(x_new - x) < epsilon:
+            return x_new
 
-        if abs(x - values[-1]) < epsilon:
-            print("x = {} after {} iterations".format(x, i + 1))
-            return x
+        x = x_new
 
-    raise ValueError("FAILURE")
+    raise ValueError("Relaxation method did not converge within the maximum number of iterations.")
+
