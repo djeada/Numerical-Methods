222e4d0ef14ce4d50e4edac0acef67a5ed2eab04
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-06-03 17:20:42 +0200

    Update golden_ratio_search.py

--- src/1_root_and_extrema_finding/golden_ratio_search/implementation/golden_ratio_search.py
+++ src/1_root_and_extrema_finding/golden_ratio_search/implementation/golden_ratio_search.py
@@ -1,25 +1,46 @@
-from scipy.constants import golden_ratio as phi
+import math
 
 
-def f(x):
-    return x + np.exp(-2 * x) - 1
+def golden_ratio_search(func, a, b, epsilon=1e-8, max_iter=100):
+    """
+    Golden Ratio Search algorithm for finding the minimum of a unimodal function.
 
+    Args:
+        func (callable): The objective function.
+        a (float): The starting point of the search interval.
+        b (float): The ending point of the search interval.
+        epsilon (float): The desired accuracy of the solution.
+        max_iter (int): The maximum number of iterations.
 
-def golden_ratio_search(f, x1=1, x4=4, num_iter=100, espilon=1e-8):
+    Returns:
+        float: The estimated minimum of the function.
 
-    x2 = x4 - (x4 - x1) / phi
-    x3 = xi + (x4 - x1) / phi
-    for i in range(num_iter):
-        if f(x2) < f(x3):
-            x4, x3 = x3, x2
-            x2 = x4 - (x4 - x1) / phi
+    Raises:
+        ValueError: If the maximum number of iterations is reached without convergence.
+    """
+    gr = (math.sqrt(5) - 1) / 2  # Golden ratio
+    x1 = b - gr * (b - a)
+    x2 = a + gr * (b - a)
+
+    f_x1 = func(x1)
+    f_x2 = func(x2)
+
+    for _ in range(max_iter):
+        if f_x1 > f_x2:
+            a = x1
+            x1 = x2
+            f_x1 = f_x2
+            x2 = a + gr * (b - a)
+            f_x2 = func(x2)
         else:
-            x1, x2 = x2, x3
-            x3 = x1 + (x4 - x1) / phi
+            b = x2
+            x2 = x1
+            f_x2 = f_x1
+            x1 = b - gr * (b - a)
+            f_x1 = func(x1)
 
-        if abs(x3 - x2) < epsilon:
-            x = (x2 + x3) / 2
-            print("x = {} after {} iterations".format(x, i + 1))
-            return x
+        if abs(b - a) < epsilon:
+            return (a + b) / 2
 
-    raise ValueError("FAILURE")
+    raise ValueError("Golden ratio search did not converge within the maximum number of iterations.")
+
