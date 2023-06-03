31be3b2e5465828692ce649088c64acd7608f85a
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-06-03 18:24:02 +0200

    Create example.py

---
+++ src/3_derivatives/taylor_series/examples/example.py
@@ -0,0 +1,10 @@
+def func(x):
+    return np.sin(x)
+
+derivatives = [np.sin, np.cos, lambda x: -np.sin(x), lambda x: -np.cos(x)]  # Up to the fourth derivative
+point = 0
+n = 5
+
+taylor_expansion = taylor_series(func, derivatives, point, n)
+print("Taylor series expansion:")
+print(taylor_expansion)
