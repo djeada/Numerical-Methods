0328bfffdbeb53a1afa9aa5b6db616e2518d751f
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2024-12-19 16:27:39 +0100

    Update golden_ratio_search.py

--- src/1_root_and_extrema_finding/golden_ratio_search/implementation/golden_ratio_search.py
+++ src/1_root_and_extrema_finding/golden_ratio_search/implementation/golden_ratio_search.py
@@ -3,25 +3,29 @@ import numpy as np
 from typing import Callable
 
 
-def golden_ratio_search(
+def golden_ratio_search_root(
     f: Callable[[float], float],
     a: float,
     b: float,
     tol: float = 1e-8,
     max_iterations: int = 1000
 ) -> float:
-    gr: float = (np.sqrt(5) + 1) / 2
-    c: float = b - (b - a) / gr
-    d: float = a + (b - a) / gr
+    gr = (np.sqrt(5) + 1) / 2
+    c = b - (b - a) / gr
+    d = a + (b - a) / gr
+
     for _ in range(max_iterations):
-        fc: float = f(c)
-        fd: float = f(d)
+        fc = abs(f(c))
+        fd = abs(f(d))
         if fc < fd:
             b, d = d, c
             c = b - (b - a) / gr
         else:
             a, c = c, d
             d = a + (b - a) / gr
+
         if np.abs(b - a) < tol:
             return (b + a) / 2
+
     raise ValueError("Golden ratio search did not converge within the maximum number of iterations.")
+
